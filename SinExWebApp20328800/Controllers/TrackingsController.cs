ee91f9513fa06c054db656942f676e0ada34ab48
syangav <syangav@gmail.com>
2017-04-12 12:32:32 +0800

    add tracking controller and view

---
+++ SinExWebApp20328800/Controllers/TrackingsController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using SinExWebApp20328800.Models;
+
+namespace SinExWebApp20328800.Controllers
+{
+    public class TrackingsController : Controller
+    {
+        private SinExWebApp20328800DatabaseContext db = new SinExWebApp20328800DatabaseContext();
+
+        // GET: Trackings
+        public ActionResult Index()
+        {
+            var trackings = db.Trackings.Include(t => t.Shipment);
+            return View(trackings.ToList());
+        }
+
+        // GET: Trackings/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Tracking tracking = db.Trackings.Find(id);
+            if (tracking == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tracking);
+        }
+
+        // GET: Trackings/Create
+        public ActionResult Create()
+        {
+            ViewBag.WaybillId = new SelectList(db.Shipments, "WaybillId", "ReferenceNumber");
+            return View();
+        }
+
+        // POST: Trackings/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "TrackingID,WaybillId,Time,Description,Location,Remark")] Tracking tracking)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Trackings.Add(tracking);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.WaybillId = new SelectList(db.Shipments, "WaybillId", "ReferenceNumber", tracking.WaybillId);
+            return View(tracking);
+        }
+
+        // GET: Trackings/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Tracking tracking = db.Trackings.Find(id);
+            if (tracking == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.WaybillId = new SelectList(db.Shipments, "WaybillId", "ReferenceNumber", tracking.WaybillId);
+            return View(tracking);
+        }
+
+        // POST: Trackings/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "TrackingID,WaybillId,Time,Description,Location,Remark")] Tracking tracking)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(tracking).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.WaybillId = new SelectList(db.Shipments, "WaybillId", "ReferenceNumber", tracking.WaybillId);
+            return View(tracking);
+        }
+
+        // GET: Trackings/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Tracking tracking = db.Trackings.Find(id);
+            if (tracking == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tracking);
+        }
+
+        // POST: Trackings/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Tracking tracking = db.Trackings.Find(id);
+            db.Trackings.Remove(tracking);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
