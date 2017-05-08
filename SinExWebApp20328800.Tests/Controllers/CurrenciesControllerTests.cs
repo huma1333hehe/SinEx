a3b378b6d845222a6900ead0c3947447df88528f
syangav <syangav@gmail.com>
2017-05-08 20:04:04 +0800

    lala

---
+++ SinExWebApp20328800.Tests/Controllers/CurrenciesControllerTests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using SinExWebApp20328800.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace SinExWebApp20328800.Controllers.Tests
+{
+    [TestFixture()]
+    public class CurrenciesControllerTests
+    {
+        [Test()]
+        public void CreateTest()
+        {
+            CurrenciesController controller = new CurrenciesController();
+            var result = controller.Create() as ViewResult;
+            Assert.That(result, Is.Not.Null);
+        }
+    }
+}
---
+++ SinExWebApp20328800.Tests/Controllers/ShipmentsControllerTests.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using SinExWebApp20328800.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SinExWebApp20328800.Controllers.Tests
+{
+    [TestFixture()]
+    public class ShipmentsControllerTests
+    {
+        [TestCase("a", "b")]
+        [TestCase("", "b")]
+        [TestCase("a", "")]
+        [TestCase("", "")]
+        public void GetPayersTest(string ShipmentPayer, string TaxPayer)
+        {
+            ShipmentsController controller = new ShipmentsController();
+            Assert.That(controller.GetPayers(ShipmentPayer, TaxPayer), Is.Not.Null);
+        }
+
+        [TestCase("")]
+        [TestCase("1")]
+        public void GetCurrentDateTimeTest(string PickupType)
+        {
+            ShipmentsController controller = new ShipmentsController();
+            Assert.That(controller.GetCurrentDateTime(PickupType), Is.Not.Null);
+        }
+    }
+}
