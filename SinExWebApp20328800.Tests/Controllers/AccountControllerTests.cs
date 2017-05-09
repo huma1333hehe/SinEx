c6cb01381387228dadd7f2c890b392ea5c01bcff
syangav <syangav@gmail.com>
2017-05-09 19:59:20 +0800

    unit test

--- SinExWebApp20328800.Tests/Controllers/AccountControllerTests.cs
+++ SinExWebApp20328800.Tests/Controllers/AccountControllerTests.cs
@@ -36,6 +36,16 @@ namespace SinExWebApp20328800.Controllers.Tests
             Assert.That(result, Is.Not.Null);
         }
 
+        [Test()]
+        public void ForgotPasswordTest()
+        {
+            AccountController controller = new AccountController();
+
+            ViewResult result = controller.ForgotPassword() as ViewResult;
+
+            Assert.IsNotNull(result);
+        }
+
         [OneTimeTearDown]
         public void TestTearDown()
         {
