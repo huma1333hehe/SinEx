9667e88b814f7db944e06575a687735f8d3a4cda
syangav <syangav@gmail.com>
2017-05-09 15:30:17 +0800

    some bugs

--- SinExWebApp20328800/Controllers/TrackingsController.cs
+++ SinExWebApp20328800/Controllers/TrackingsController.cs
@@ -85,7 +85,7 @@ namespace SinExWebApp20328800.Controllers
         }
 
         // GET: Trackings/Create
-        [Authorize(Roles = "Employee")]
+
         public ActionResult Create(int? WaybillId, bool? terminated)
         {
 
